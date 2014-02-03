cd7f0f79fd3e4b809276b555f8e1842bb51458d8
Jongwon Chae <jwchae@reallplay.com>
2014-02-04 00:44:27 +0900

    application helper upadete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include PublicActivity::StoreController
   
-  private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
 end
