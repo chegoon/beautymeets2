6718b6abf52aa2e0c6e154cce47b349cd48a1a16
Jongwon Chae <jwchae@reallplay.com>
2014-03-12 18:36:39 +0900

    impression admin added

--- app/admin/impression.rb
+++ app/admin/impression.rb
@@ -2,8 +2,8 @@ ActiveAdmin.register Impression do
 	index do 
 		column :id
 		column :impressionable_type
-		column :impressionable_id
-		column :action_name
+		column :impressionable
+		#column :action_name
 		column :user_id
 		column :referrer
 		column :created_at
