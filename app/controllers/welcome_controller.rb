62de0761880945311c9807bc7a577f2da276ed47
Jongwon Chae <jwchae@reallplay.com>
2014-03-30 21:06:00 +0900

    welcome controller updated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -20,23 +20,51 @@ class WelcomeController < ApplicationController
 =end
 			
 			#@events = Event.where('released_at <= ? AND finish_on >= ? ', Time.now, Time.now).order("created_at DESC")
-	    @best_tutorials = Tutorial.where(published: true, created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(2)
+	    #@best_tutorials = Tutorial.where(published: true, created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(2)
+	    @best_tutorials = Tutorial.where("id IN (?)", Impression.find_by_sql("SELECT impressionable_id 
+FROM impressions 
+WHERE impressionable_type = 'Tutorial'
+AND (created_at > CURDATE() - INTERVAL 2 WEEK)
+GROUP BY impressionable_id 
+ORDER BY count(impressionable_id)  DESC
+LIMIT 2").map(&:impressionable_id))
 	    @latest_tutorials = Tutorial.where(published: true).order("created_at DESC, updated_at DESC").limit(2)
 	    #@best_beautyclasses = Beautyclass.where(published: true, closed: false).order("view_count DESC, updated_at DESC").limit(2)
 	    #@best_videos = Video.joins(:video_group).where("video_groups.published=TRUE AND videos.published=TRUE").unread_by(current_user).order("view_count DESC, created_at DESC").limit(4)
 	    #@latest_videos = Video.joins(:video_group).where("video_groups.published=TRUE AND videos.published=TRUE").unread_by(current_user).order("published_at DESC").limit(4)
-	    @best_items = Item.where(created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(4)
+	    #@best_items = Item.where(created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(4)
+	    @best_items = Item.where("id IN (?)", Impression.find_by_sql("SELECT impressionable_id 
+FROM impressions 
+WHERE impressionable_type = 'Item'
+AND (created_at > CURDATE() - INTERVAL 2 WEEK)
+GROUP BY impressionable_id 
+ORDER BY count(impressionable_id)  DESC
+LIMIT 2").map(&:impressionable_id))
 			@latest_items = Item.where("id IN (?)", Itemization.pluck(:item_id)).order("created_at DESC, updated_at DESC").limit(4)
 	    @latest_posts = Post.where(published: true).order("created_at DESC, updated_at DESC").limit(3)
 	    @bookmarks = Bookmark.where(user_id: @user.id)
 	  else
 	  	#@events = Event.where('released_at <= ? AND finish_on >= ? ', Time.now, Time.now).order("created_at DESC")
-	    @best_tutorials = Tutorial.where(published: true, created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(2)
+	    #@best_tutorials = Tutorial.where(published: true, created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(2)
+	    @best_tutorials = Tutorial.where("id IN (?)", Impression.find_by_sql("SELECT impressionable_id 
+FROM impressions 
+WHERE impressionable_type = 'Tutorial'
+AND (created_at > CURDATE() - INTERVAL 2 WEEK)
+GROUP BY impressionable_id 
+ORDER BY count(impressionable_id)  DESC
+LIMIT 2").map(&:impressionable_id))
 	    @latest_tutorials = Tutorial.where(published: true).order("created_at DESC, updated_at DESC").limit(2)
 	    #@best_beautyclasses = Beautyclass.where(published: true, closed: false).order("view_count DESC, created_at DESC").limit(2)
 	    #@best_videos = Video.joins(:video_group).where("video_groups.published=TRUE AND videos.published=TRUE").order("view_count DESC, created_at DESC").limit(4)
 	    #@latest_videos = Video.joins(:video_group).where("video_groups.published=TRUE AND videos.published=TRUE").order("published_at DESC").limit(4)
-	    @best_items = Item.where(created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(4)
+	    #@best_items = Item.where(created_at: (8.week.ago)..(Time.now), updated_at: (2.week.ago)..(Time.now)).order("view_count DESC, created_at DESC").limit(4)
+	    @best_items = Item.where("id IN (?)", Impression.find_by_sql("SELECT impressionable_id 
+FROM impressions 
+WHERE impressionable_type = 'Item'
+AND (created_at > CURDATE() - INTERVAL 2 WEEK)
+GROUP BY impressionable_id 
+ORDER BY count(impressionable_id)  DESC
+LIMIT 2").map(&:impressionable_id))
 			@latest_items = Item.where("id IN (?)", Itemization.pluck(:item_id)).order("created_at DESC, updated_at DESC").limit(4)
 			@latest_posts = Post.where(published: true).order("created_at DESC, updated_at DESC").limit(3)
 	  end
