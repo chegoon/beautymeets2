882bff5563f163bdcdce2e4e15a4a29724e478f5
Jongwon Chae <jongwon.chae@gmail.com>
2014-05-30 17:14:01 +0900

    skin info updated

--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -1,22 +1,22 @@
 class UserStepsController < ApplicationController
-  include Wicked::Wizard
-  before_filter :set_user_steps
-  steps :skin_type, :skin_tone, :skin_trouble
-  
-  def show
-    render_wizard
-  end
-  
-  def update
-	  @member.attributes = params[:member]
+	include Wicked::Wizard
+	before_filter :set_user_steps
+	steps :step1, :step2, :step_final
+	
+	def show
+		render_wizard
+	end
+	
+	def update
+		@member.attributes = params[:member]
 
-	  render_wizard @member
+		render_wizard @member
 	end
 
 
 	private
 	def set_user_steps
-  	@user = current_user
-  	@member = @user.profile
+		@user = current_user
+		@member = @user.profile
 	end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -8,9 +8,9 @@ class Member < ActiveRecord::Base
   include Authority::Abilities
   self.authorizer_name = 'MemberAuthorizer'
 
-  acts_as_taggable_on :skin_types, :skin_troubles, :skin_tones
+  acts_as_taggable_on :skin_types, :skin_troubles, :skin_tones, :skin_brightnesses, :skin_colors
 
-  attr_accessible :year_of_birth, :gender_id, :slug, :skin_type_list, :skin_trouble_list, :skin_tone_list
+  attr_accessible :year_of_birth, :gender_id, :slug, :skin_type_list, :skin_trouble_list, :skin_tone_list, :skin_brightness_list, :skin_color_list
 
   #validates :year_of_birth, :length => { :is => 4 }, numericality: { :only_integer => true, :greater_than_or_equal_to => 1970, :less_than_or_equal_to => 2010 }, presence: true
   
