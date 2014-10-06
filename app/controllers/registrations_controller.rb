5b93af7aafc7cd8b56f5e3f0e44dd1b07c8cf0f0
Jongwon Chae <jongwon.chae@gmail.com>
2014-10-07 04:32:20 +0900

    reg controller updated

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,37 @@
 class RegistrationsController < Devise::RegistrationsController
- #before_filter :set_account
+
+	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+	before_filter :cors_preflight_check, :if => Proc.new { |c| c.request.format == 'application/json' }
+	after_filter :set_access_control_headers, :if => Proc.new { |c| c.request.format == 'application/json' }
+	after_filter :set_csrf_cookie_for_ng, :if => Proc.new { |c| c.request.format == 'application/json' }
+	
+	# This is used to allow the cross origin POST requests made by app.
+	def set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+		headers['Access-Control-Max-Age'] = "1728000"
+	end
+
+	# If this is a preflight OPTIONS request, then short-circuit the
+	# request, return only the necessary headers and return an empty
+	# text/plain.
+
+	def cors_preflight_check
+		if request.method == :options
+			headers['Access-Control-Allow-Origin'] = '*'
+			headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+			headers['Access-Control-Request-Method'] = '*'
+			headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+			headers['Access-Control-Max-Age'] = '1728000'
+			render :text => '', :content_type => 'text/plain'
+		end
+	end
+
+	def set_csrf_cookie_for_ng
+		cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+	end
 
 	def create
 		
