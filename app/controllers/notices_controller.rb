49ee156b7a8126c9085dae511a24ad9b9baa9e54
Jongwon Chae <jongwon.chae@gmail.com>
2014-10-13 14:47:40 +0900

    notice controller updated

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -30,14 +30,17 @@ class NoticesController < ApplicationController #InheritedResources::Base
 		@notices = Notice.all
 		
 		@commentable = @notice
-		@comments = @commentable.root_comments.order("created_at ASC")
-		
+		comments_per_page = 7
+		page_index = params[:page] ? params[:page] : @commentable.root_comments.order("created_at ASC").paginate(:page => params[:page], :per_page => comments_per_page).total_pages
+		@comments = @commentable.root_comments.order("created_at ASC").page(page_index).per_page(comments_per_page)
+
 		if user_signed_in?
-			@comment = Comment.build_from(@commentable, current_user.id, "") 
-			@notice.mark_as_read! :for => current_user
+			@comment = @commentable.comments.new(user_id: current_user.id)
+			@comment.build_picture 
 		else
 			@comment = Comment.new
 		end
+
 
 		super
 
