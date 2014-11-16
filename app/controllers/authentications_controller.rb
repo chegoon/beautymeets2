bc90c5679c94c3eb0951a0f83a3884bbc2427520
Jongwon Chae <jongwon.chae@gmail.com>
2014-11-16 14:30:52 +0900

    api auth updated

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationsController < ApplicationController
 	# GET /authentications
 	# GET /authentications.json
 
-	before_filter :default_format_check, only: [:create]
+	#before_filter :default_format_check, only: [:create]
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	before_filter :cors_preflight_check, :if => Proc.new { |c| c.request.format == 'application/json' }
 	after_filter :set_access_control_headers,  :if => Proc.new { |c| c.request.format == 'application/json' } 
@@ -67,11 +67,8 @@ class AuthenticationsController < ApplicationController
 			@auth = authentication
 			
 			respond_to do |format|
-				if (session[:request_format].present? && session[:request_format] == "json")
-					format.json { redirect_to authentication_path(id: authentication.id, format: :json,  request_format: "json", authentication_token: params[:authToken])}
-				else 
+					format.json { sign_in_and_redirect(:user, authentication.user) }
 					format.html { sign_in_and_redirect(:user, authentication.user) }
-				end
 				#format.json { redirect_to "authentications/" + authentication.id + "?request_format='json'&authentication_token=" + params[:authToken] }
 				#format.json { render json: {status: 200, success: true} }
 			end	
