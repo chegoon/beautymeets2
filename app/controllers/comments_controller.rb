badc430092cf2778970047a4d8a9d82a8fdfe1e4
Jongwon Chae <jongwon.chae@gmail.com>
2014-11-23 12:46:30 +0900

    quick fix for api

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -52,14 +52,14 @@ class CommentsController < ApplicationController
 
 			devices = Array.new
 			@commentable.comments.each do |c|
-				if current_user.id <> c.user_id
+				if current_user.id == c.user_id
 					@comment.create_activity :create, owner: current_user, recipient: c.user
 					c.user.devices.each do |d|
 						devices << d.uuid
 					end
 				end
 			end
-			PushNotificationSender.notify_devices({message: @commentable.title + "에 댓글이 달렸습니다.", device_type: 3, devices: {devices}}
+			#PushNotificationSender.notify_devices({message: @commentable.title + "에 댓글이 달렸습니다.", device_type: 3, devices: {devices}}
 				
 			if !(@commentable.class.name == "Notice")
 				respond_to do |format|
