90c91b7f63ba25188bcd91704de5a324907b7d1a
Jongwon Chae <jongwon.chae@gmail.com>
2014-11-25 17:00:43 +0900

    event api created

--- config/routes.rb
+++ config/routes.rb
@@ -41,8 +41,8 @@ Beautymeets2::Application.routes.draw do
 	# Set the default format to json
 	namespace :api,  defaults: {format: :json} do 
 		namespace :v1 do 
-			match "events", defaults: { format: :json}
-			
+			resources "events", defaults: { format: :json}
+
 			match "devices" => "devices#create", defaults: { format: :json }
 			match "me" => "users#index"
 
