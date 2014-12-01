60bc3949e5138ad4e08a3756fb46bfac4b11f348
Jongwon Chae <jongwon.chae@gmail.com>
2014-12-01 23:11:31 +0900

    comment api updated

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -36,7 +36,7 @@ module API
 					#puts "picture: #{uploaded_file}"
 					comment = JSON.parse(params[:comment])
 					#@comment = @commentable.comments.new({user_id: @user.id, body: params[:comment][:body], picture_attributes: {picture_attributes: params[:image]}}) #picture: Picture.new(uploaded_file)})
-					@comment = @commentable.comments.new({user_id: @user.id, body: comment.body, picture_attributes: {image: params[:image]}}) 
+					@comment = @commentable.comments.new({user_id: @user.id, body: comment['body'], picture_attributes: {image: params[:image]}}) 
 					#@comment.picture.image = uploaded_file
 
 					if @comment.save
