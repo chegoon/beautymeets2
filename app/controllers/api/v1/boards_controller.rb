27792e0fd8050f3895ca204cd9ba315f28af58aa
Jongwon Chae <jongwon.chae@gmail.com>
2014-12-02 14:14:36 +0900

    push notification  api updated

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -44,7 +44,7 @@ module API
 			def create
 				if params[:image]
 					board_params = JSON.parse(params[:board])
-					@board = @user.boards.new({user_id: @user.id, title: board_params['title'],  description: board_params['description'], picture_attributes: {image: params[:image]}}) 
+					@board = @user.boards.new({user_id: @user.id, title: board_params['title'],  description: board_params['description'], picture_attributes: {image: board_params['image']}}) 
 				else
 					@board = @user.boards.create({title: params[:board][:title], description: params[:board][:description]})	
 				end
