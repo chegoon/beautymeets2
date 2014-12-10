e7ef866eadb56ae94eab8cc3633cf5cecffabeb2
Jongwon Chae <jongwon.chae@gmail.com>
2014-12-10 18:41:12 +0900

    new app download strategy adapted

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,8 +22,17 @@ class WelcomeController < ApplicationController
 	def index
 
 		#agent = request.headers["HTTP_USER_AGENT"].downcase
-		#if agent == "android"
-		#else
+		if @android_detected == true
+
+			@events = Event.where('released_at <= ? AND announcement_closed_at >= ? AND published = TRUE', Time.now, Time.now).order("created_at DESC")
+			@best_tutorials = ""
+			@latest_tutorials = Tutorial.where(published: true).order("created_at DESC, updated_at DESC").limit(2)
+			@best_items = ""
+			@latest_items = Item.where("id IN (?)", Itemization.pluck(:item_id)).order("created_at DESC, updated_at DESC").limit(4)
+			@latest_posts = Post.where(published: true).order("created_at DESC, updated_at DESC").limit(3)
+			@tutorials = ""
+
+		else
 			if user_signed_in?
 			
 				@user = current_user
