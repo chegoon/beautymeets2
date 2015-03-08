76890316ef7558072857f97e8a5dd0135c1dbb75
Jongwon Chae <jongwon.chae@gmail.com>
2015-03-08 21:47:16 +0900

    model location, shop deleted

--- app/models/shop.rb
+++
@@ -1,7 +0,0 @@
-class Shop < ActiveRecord::Base
-  attr_accessible  :description, :name, :location_attributes
-  has_many :beautyclasses
-  has_one :location
-
-  accepts_nested_attributes_for :location, :allow_destroy => true
-end
