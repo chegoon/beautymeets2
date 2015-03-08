0efb555e2b7b26773fe5a7e09525944fded15ce1
Jongwon Chae <jongwon.chae@gmail.com>
2015-03-09 01:23:46 +0900

    global mailer added

---
+++ app/mailers/global_mailer.rb
@@ -0,0 +1,13 @@
+class GlobalMailer < ActionMailer::Base
+	helper :application # gives access to all helpers defined within `application_helper`.
+
+	#default to: "hellobeauty@reallplay.com"
+	
+	def report(sender, title, description)
+		@sender = sender
+		@title = title
+		@description = description
+
+		mail(:to => "hellobeauty@reallplay.com", :from => (@sender.email || @sender.name), :subject => @title, :description => @description)
+	end
+end
