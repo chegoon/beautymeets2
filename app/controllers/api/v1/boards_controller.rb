998ce216a3dca1d7c72de22f61fe53c49d03962b
Jongwon Chae <jongwon.chae@gmail.com>
2015-03-17 15:23:01 +0900

    image upload api update

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -48,7 +48,7 @@ module API
 						board_params = JSON.parse(params[:board]) 
 						@board = @user.boards.new({user_id: @user.id, title: board_params['title'],  description: board_params['description'], picture_attributes: {image: params[:image]}}) 
 					else
-						@board = @user.boards.new({user_id: @user.id, title: JSON.parse(params[:title]),  description: JSON.parse(params[:description]), picture_attributes: {image: params[:image]}}) 
+						@board = @user.boards.new({user_id: @user.id, title: params[:title],  description: params[:description], picture_attributes: {image: params[:image]}}) 
 					end					
 				else
 					@board = @user.boards.create({title: params[:board][:title], description: params[:board][:description]})	
