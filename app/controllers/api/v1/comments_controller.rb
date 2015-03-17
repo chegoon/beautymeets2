80a55cf3ea623cc5d43b0bfd159d60d4aab4cded
Jongwon Chae <jongwon.chae@gmail.com>
2015-03-17 16:55:28 +0900

    image upload api update

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -57,7 +57,7 @@ module API
 						@comment = @commentable.comments.new({user_id: @user.id, body: comment_params['body'], picture_attributes: {image: params[:image]}}) 
 						@parent = Comment.find(comment_params['parent_id']) if comment_params['parent_id']
 					else
-						@comment = @commentable.comments.new({user_id: @user.id, body: JSON.parse(params[:body])['body'], picture_attributes: {image: params[:image]}}) 
+						@comment = @commentable.comments.new({user_id: @user.id, body: params[:body], picture_attributes: {image: params[:image]}}) 
 						@parent = Comment.find(JSON.parse(params[:parent_id])['parent_id']) if params[:parent_id]
 					end
 				else
